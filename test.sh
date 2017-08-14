f76c3e7917a45b9b4899cee70b464f56bf079fae
qeed <qeed.quan@gmail.com>
2017-08-14 06:48:07 -0400

    bashify

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-sh build.sh
+bash build.sh
 for i in input/*.pl0
 do
     ./pl0 $i 
-    if [[ $? != 0 ]]
+    if [[ "$?" != "0" ]]
     then
         echo "test failed:" "$i"
         exit 1
